43be4469ece8082f789b538e98207bcfddba88a2
Gustavo <u20221c138@upc.edu.pe>
2024-09-25 14:42:44 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { RegistrationPageComponent } from "./iam/pages/registration-page/registration-page.component";
+import { LoginPageComponent } from "./iam/pages/login-page/login-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'register', component: RegistrationPageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
