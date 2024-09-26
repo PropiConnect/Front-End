02b1fac67379fa1e8144dd91c003b1224c0ff892
Oscar Antayhua <oscarjac2001@gmail.com>
2024-09-25 21:03:09 -0500

    feat(app): added payment route.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import {RegistrationFormComponent} from "./iam/components/registration-form/registration-form.component";
 import {LoginFormComponent} from "./iam/components/login-form/login-form.component";
+import {PaymentFormComponent} from "./iam/components/payment-form/payment-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrationFormComponent, LoginFormComponent],
+  imports: [RouterOutlet, RegistrationFormComponent, LoginFormComponent, PaymentFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
