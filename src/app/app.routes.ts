8691db461e1abf7c3762f5c7a9e5aa6e6a99e108
Gustavo <u20221c138@upc.edu.pe>
2024-10-21 23:15:42 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import {PaymentFormComponent} from "./payments/components/payment-form/payment-f
 import {PaymentRecordsComponent} from "./payments/pages/payment-records/payment-records.component";
 import {CardProfileComponent} from "./iam/components/card-profile/card-profile.component";
 import {MyPropertiesComponent} from "./properties/pages/my-properties/my-properties.component";
+import {PropertiesManagementComponent} from "./properties/pages/properties-management/properties-management.component"
+import {CreatePropertiesComponent} from "./properties/pages/create-properties/create-properties.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -14,5 +16,7 @@ export const routes: Routes = [
   { path: 'payment', component: PaymentFormComponent },
   { path: 'properties', component: MyPropertiesComponent },
   { path: 'owner/payments', component: PaymentRecordsComponent },
-  { path: 'profile/:id', component: CardProfileComponent }
+  { path: 'profile/:id', component: CardProfileComponent },
+  { path: 'all-properties', component: PropertiesManagementComponent},
+  { path: 'create-properties', component: CreatePropertiesComponent }
 ];
