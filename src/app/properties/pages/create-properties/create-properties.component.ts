786b0d963c798d877185172153b2a9ec19323b88
Oscar Antayhua <oscarjac2001@gmail.com>
2024-11-20 21:49:22 -0500

    feat(create-properties): update.

--- src/app/properties/pages/create-properties/create-properties.component.ts
+++ src/app/properties/pages/create-properties/create-properties.component.ts
@@ -1,31 +1,50 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
-import { FormsModule } from '@angular/forms'; // To use ngModel
-import {HttpClient, HttpClientModule} from '@angular/common/http'; // Ensure HttpClientModule is imported
-import { PropertiesService} from "../../services/properties.service";
-import {Properties} from "../../model/properties.entity";
-import {MatTableDataSource} from "@angular/material/table";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PropertiesService } from "../../services/properties.service";
+import { Properties } from "../../model/properties.entity";
+import { MatTableDataSource } from "@angular/material/table";
 
 @Component({
   selector: 'app-create-properties',
-  standalone: true, // This component is standalone
-  imports: [FormsModule, HttpClientModule], // Ensure FormsModule and HttpClientModule are included
+  standalone: true, // Este componente es standalone
+  imports: [
+    FormsModule,
+    HttpClientModule // Usa HttpClientModule para habilitar HttpClient
+  ],
+  providers: [
+    PropertiesService // Asegúrate de registrar el servicio si es necesario
+  ],
   templateUrl: './create-properties.component.html',
   styleUrls: ['./create-properties.component.css']
 })
 export class CreatePropertiesComponent implements OnInit, AfterViewInit {
-
   protected property: Properties;
-  protected columsToDisplay: string[] = ['id', 'owner', 'ownerId', 'city', 'type', 'addres', 'description', 'propertyType', 'rentalType', 'image', 'initf'];
+  protected columnsToDisplay: string[] = [
+    'id',
+    'owner',
+    'ownerId',
+    'city',
+    'type',
+    'address',
+    'description',
+    'propertyType',
+    'rentalType',
+    'image',
+    'initialPrice'
+  ];
   protected editMode: boolean = false;
   protected dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
 
-  constructor(private propertyService: PropertiesService, private http: HttpClient) {
+  constructor(private propertyService: PropertiesService) {
     this.property = new Properties({});
   }
 
   ngOnInit() {
+    console.log('CreatePropertiesComponent initialized');
   }
 
   ngAfterViewInit() {
+    console.log('View initialized');
   }
 }
